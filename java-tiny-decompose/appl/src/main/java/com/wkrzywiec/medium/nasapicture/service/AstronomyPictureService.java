e9d04d9c1083635e4a429eb3738bbf3f16bb66ed
eugene <eugene.r.w.12@gmail.com>
2019-11-24 02:14:22 -0500

    added check yesterday post option

--- java-tiny-decompose/appl/src/main/java/com/wkrzywiec/medium/nasapicture/service/AstronomyPictureService.java
+++ java-tiny-decompose/appl/src/main/java/com/wkrzywiec/medium/nasapicture/service/AstronomyPictureService.java
@@ -1,5 +1,10 @@
 package com.wkrzywiec.medium.nasapicture.service;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import com.wkrzywiec.medium.nasapicture.model.AstronomyPictureOfDay;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -18,8 +23,25 @@ public class AstronomyPictureService {
                 AstronomyPictureOfDay.class);
     }
 
+    public AstronomyPictureOfDay getDescription() {
+        return restTemplate.getForObject(
+                "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&hd&date="+getYesterdayDateString(),
+                AstronomyPictureOfDay.class);
+    }
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    private Date yesterday() {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, -1);
+        return cal.getTime();
+    }
+    
+    private String getYesterdayDateString() {
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            return dateFormat.format(yesterday());
     }
 }
